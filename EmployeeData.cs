642cc6d69c78dc45e5ae961e17586fe325b677d5
Marshall Simien <msimien3@gmail.com>
2021-04-13 00:01:37 -0400

    Add project files.

---
+++ EmployeeData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    //container for Code
+    class EmployeeData
+    {
+        // Public class definition for Employee functions.
+        public class Employee
+        { 
+            public static int employeeCounter; // Define employee incremental counter.
+
+            // Function for writing out employee name & ID.
+            public static void Employee1(string name, string id)
+            {
+                Console.WriteLine("Employee Name: " + name);
+                Console.WriteLine("Employee ID: " + id);
+            }
+
+            // Function that increments the employee counter by 1.
+            public static int AddEmployee()
+            {
+                return ++employeeCounter;
+            }
+        }
+
+        // Main function for where instructions are placed for computer to execute.
+        static void Main()
+        {
+            Console.Write("Enter employee name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter employee ID: ");
+            string id = Console.ReadLine();
+            Console.Write("Enter the current number of employees: ");
+            string n = Console.ReadLine();
+
+            Employee.employeeCounter = Int32.Parse(n);  // Converts string entry to integer 32 bit.
+            Employee.AddEmployee();
+
+            Console.WriteLine();    // Line break
+            Employee.Employee1(name, id);   // Outputs employee name & ID function.
+            Console.WriteLine("Number of current employees: " + Employee.employeeCounter);  // Outputs employee counter.
+            Console.ReadLine();    //Keeps console window up after compiling the code. Waits for an entry of text.
+        }
+    }
+}
---
+++ Free_Write.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// place where code is executed. Within the project "Tutorial" 
+ namespace Tutorial
+{
+    //container for Code
+    class Free_Write
+    {
+        // Main function for where instructions are placed for computer to execute.
+        static void Main(string[] args)
+        {
+            // 1) Print out line of text.
+            Console.WriteLine("1) Hello Marsh!");
+            Console.WriteLine("Yo");
+
+            // Create a string variable
+            string testing = "2) This is just a test";
+            
+            // 2) Print out string variable.
+            Console.WriteLine(testing);
+                // Returns a true value if string contains the following phrase.
+                Console.WriteLine(testing.Contains("This"));
+                // Returns the seconds character of the string.
+                Console.WriteLine(testing[1]);
+                // Returns the 4 characters of the string testing starting at index 11. Index includes spaces.
+                Console.WriteLine(testing.Substring(11, 4));
+
+            // 3) Print out a floating point number.
+            double test_number = 4.54;
+            Console.WriteLine("3) " + test_number);
+
+            // 4) Allow user to enter a line of text.
+            //This line allows a user to write a line of text without the program jumping to the next executing line. 
+            Console.Write("4) Enter your name here: ");
+            // Stores the line of text entered as a string.
+            string name = Console.ReadLine();
+            // Repeat nomenclature for entering age.
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            // Printout entered text.
+            Console.WriteLine("Hello " + name + ", you are " + age + " years old!");
+
+            // 5) create an array of inegers.
+            int[] array1 = { 1, 2, 3, 4, 5, 6 };
+            array1[1] = 6000;
+            //Printout.
+            Console.WriteLine(array1[2]);
+            Console.WriteLine(array1[1]);
+
+            //Keeps console window up after compiling the code. Waits for an entry of text.
+            Console.ReadLine();  
+        }
+    }
+} 
