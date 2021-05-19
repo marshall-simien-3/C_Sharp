598fd03316bb72812d6744aa1cf47cd73313002e
Marshall Simien <msimien3@gmail.com>
2021-05-18 22:31:47 -0400

    5/18 10:31pm

---
+++ Classes/Arrays/Arrays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training_C_Sharp.Classes.Arrays
+{
+    public class Arrays
+    {
+        // Array name
+        public string[] my_family;
+
+        // Define array size 3.
+        public Arrays()
+        {
+            my_family = new string[3] { "Marsh", "Baylee", "Wolf" };
+        }
+
+        // Resizing the array.
+        public void upsize_array(int newSize)
+        {
+            Array.Resize(ref my_family, newSize);
+        }
+    }
+}
+
---
+++ Source Code/Arrays_Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training_C_Sharp.Classes.Arrays;
+
+// place where code is executed. Within the project.
+namespace Training_C_Sharp
+{
+
+    //container for Code
+    class Arrays_Main
+    {
+        // Main function for where instructions are placed for computer to execute.
+        static void Main()
+        {
+            // 1) Call methods from Arrays class.
+            Arrays one_array = new Arrays();
+            Console.WriteLine($"1) My name is {one_array.my_family[0]}, my lover's name is {one_array.my_family[1]}, and my dog's name is {one_array.my_family[2]}.");
+            one_array.upsize_array(5); // Upsize the current array.
+            one_array.my_family[3] = "Auggie";
+            one_array.my_family[4] = "Grace";
+            Console.WriteLine($"My brother is {one_array.my_family[3]}, and my sister is {one_array.my_family[4]}.");
+            Console.WriteLine($"Length of array: {one_array.my_family.Length}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
