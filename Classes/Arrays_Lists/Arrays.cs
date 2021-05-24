befe8c55e882c7638991301f4acf38022f6fe02d
Marshall Simien <msimien3@gmail.com>
2021-05-24 00:32:12 -0400

    12:32 5/24

--- Classes/Arrays_Lists/Arrays.cs
+++ Classes/Arrays_Lists/Arrays.cs
@@ -6,8 +6,9 @@ namespace Training_C_Sharp.Classes.Arrays_Lists
 {
     public class Arrays
     {
-        // Array name
+        // Array names
         public string[] my_family;
+        public double[] family_ages;
 
         /// <summary>
         /// Constructor. Define array size 3.
@@ -23,6 +24,11 @@ namespace Training_C_Sharp.Classes.Arrays_Lists
         public void upsize_array(int newSize)
         {
             Array.Resize(ref my_family, newSize);
+        }
+
+        public void Arrays_2()
+        {
+            family_ages = new double[3] { 25, 16, 21 };
         }
     }
 }
--- Source Code/Arrays_Lists_Main.cs
+++ Source Code/Arrays_Lists_Main.cs
@@ -22,19 +22,32 @@ namespace Training_C_Sharp
             one_array.my_family[3] = "Auggie";
             one_array.my_family[4] = "Grace";
             Console.WriteLine($"My brother is {one_array.my_family[3]}, and my sister is {one_array.my_family[4]}.");
-            Console.WriteLine($"Length of array: {one_array.my_family.Length}\n\r");
+            Console.WriteLine($"Length of array: {one_array.my_family.Length}");
+
+            var two_array = new Arrays();
+            two_array.Arrays_2();
+            Console.WriteLine($"Average of family ages: {two_array.family_ages.Average()}\n\r");
 
 
             // 2) Call methods from Lists class.
             var two_list = new Lists();
             Console.Write("2) List of all the coil widths: "); 
-            Console.WriteLine(String.Join(", ", two_list.coil_widths)); // Comma separates items in list once outputted.
+            Console.WriteLine(String.Join(", ", two_list.coil_widths)); // Comma separates all items in list once outputted.
             Console.WriteLine($"Is this width contained within list? Answer: {two_list.normal_widths(24)}\n\r");
 
      
             // 3) Create another list. 
             var three_list = new List<string> { "Me", "Myself", "I" };
-            Console.WriteLine($"3) List of all names: " + String.Join(", ", three_list)); // Comma separates items in list once outputted.
+            Console.WriteLine($"3) List of all names: " + String.Join(", ", three_list) + "\n\r"); // Comma separates all items in list once outputted.
+
+            // 4) Create a dictionary.
+            var four_dictionary_words = new Dictionary<string, string>();
+            four_dictionary_words.Add("cap", "to bluff or not truthful"); // Dictionary structure: ("[word]", "[definition]")
+            four_dictionary_words.Add("lit", "to get hyped");
+
+            List<string> four_keys = four_dictionary_words.Keys.ToList();
+            Console.WriteLine($"4) List the dictionary keys: {String.Join(", ", four_keys)}");
+            Console.WriteLine($"Dictionary key count: {four_keys.Count}");
 
             Console.ReadLine();
 
