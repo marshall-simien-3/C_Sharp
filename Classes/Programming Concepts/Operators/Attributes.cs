91b959e49dcf93895f3fd916b171f94426cf5d84
Marshall Simien <msimien3@gmail.com>
2021-08-09 23:34:42 -0400

    8/9 11:30pm

---
+++ Classes/Programming Concepts/Operators/Attributes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training_C_Sharp.Classes.Programming_Concepts.Operators
+{
+    // Attributes allow you to write declarative info to you code. Attributes can use parameters.
+    public static class Attributes
+    {
+        /// <summary>
+        /// Declare the below function as obsolete. There is a replacement function below it accepting an int List as a parameter. This will be used instead.
+        /// </summary>
+        /// <param name="firstMult"></param>
+        /// <param name="SecondMult"></param>
+        /// <returns></returns>
+        [Obsolete("Use the Multiplier function with List parameter passed.")]
+        public static int Multiplier(int firstMult, int SecondMult)
+        {
+            return firstMult * SecondMult;
+        }
+
+        /// <summary>
+        /// Function to use in place of obsolete function.
+        /// </summary>
+        /// <param name="multList"></param>
+        /// <returns></returns>
+        public static int Multiplier (List<int> multList)
+        {
+            int Sum = 1;
+            foreach(int Number in multList)
+            {
+                Sum = Sum * Number;
+            }
+            return Sum;
+        }
+    }
+}
---
+++ Source Code/Programming Concepts/Operators/Attributes_Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training_C_Sharp.Classes.Programming_Concepts.Operators;
+
+namespace Training_C_Sharp
+{
+    class Attributes_Main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Attribute Example: Obsolete");
+            Console.WriteLine(Attributes.Multiplier(new List<int>() { 3, 2, 4, 5 }) + "\n\r");
+            Console.ReadLine();
+        }
+    }
+}
